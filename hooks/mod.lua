c1bd16234cdf5387c95f71f8eb85083777909336
Marcos Hebestreit <marcos.hebestreit@gmail.com>
2026-09-23 00:53:20 +0200

    build firerateramp, need triggerheld for spintime

--- hooks/mod.lua
+++ hooks/mod.lua
@@ -1,14 +1,16 @@
 local SD = _G.SpinDrive
 
-local stats = {
-	accel_time = 0.80,
-	decel_time = 1.00,
-	base_rpm = 3000,
-	peak_rpm = 4500,
-	rpm_pct_min = 0.13,
-	accuracy_penalty = 0.5
+local wpn_upg_b_gat = {
+	custom_stats = {
+		spindrive = {
+			accel_time = 0.80,
+			decel_time = 1.00,
+			base_rpm = 3000,
+			peak_rpm = 4500,
+			rpm_pct_min = 0.13,
+			accuracy_penalty = 0.5
+		}
+	}
 }
 
-
-SD.register("name", stats)
 
--- hooks/spindrive_core.lua
+++ hooks/spindrive_core.lua
@@ -1,8 +1,38 @@
 _G.SpinDrive = _G.SpinDrive or {}
 local SD = _G.SpinDrive
 
-SD.registeredMods = SD.registeredMods or {}
+function getPartStats(weapon)
+	for _, partId in pairs(weapon._blueprint) do
+		local part = tweak_data.weapon.factory.parts[partId]
+		local stats = (part.custom_stats or {}).spindrive
+		if stats then
+			return stats
+		end
+	end
+	return nil
+end
 
-function SD.register(name, stats)
-	table.insert(SD.registeredMods, stats)
+local function lerp(min, max, pct)
+	return min + ((max - min) * pct)
+end
+
+local function rampFRM(weapon)
+	
+	pct
+	
+	local stats = getPartStats(weapon)
+	if stats then
+		local desiredRPM = lerp(stats.base_rpm * stats.rpm_pct_min, stats.peak_rpm, pct)
+		return desiredRPM / stats.base_rpm
+	end
+
+	return 1
+end
+
+Hook:PreHook(NewRaycastWeaponBase, "fire_rate_multiplier", "spindrive_FRM", function(self,...)
+	local FRM = rampFRM(self)
+	if FRM > 1
+		return FRM
+	end
+	return self._fire_rate_multiplier
 end
