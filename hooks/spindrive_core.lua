f2d11309e212e3dcf8c3e9c1ccc8f98320957fe9
Marcos Hebestreit <marcos.hebestreit@gmail.com>
2026-09-23 21:30:47 +0200

    implemented spinning

--- hooks/spindrive_core.lua
+++ hooks/spindrive_core.lua
@@ -2,9 +2,13 @@ _G.SpinDrive = _G.SpinDrive or {}
 local SD = _G.SpinDrive
 
 function SD.getPartStats(weapon)
-	local part = tweak_data.weapon.factory.parts[weapon._blueprint.barrel]
-	local stats = (part.custom_stats or {}).spindrive
-	return stats
+	for slot, partId in pairs(weapon._blueprint) do
+		local part = tweak_data.weapon.factory.parts[partId]
+		local stats = (part.custom_stats or {}).spindrive
+		if stats then
+			return partId, stats
+		end
+	end
 end
 
 Hooks:OverrideFunction(NewRaycastWeaponBase, "fire_rate_multiplier", function(self, ...)
@@ -13,10 +17,12 @@ end)
 
 Hooks:PostHook(PlayerInventory, "_send_equipped_weapon", "spindrive_init", function(self,...)
 	local base = self:equipped_unit():base()
-    local stats = _G.SpinDrive.getPartStats(base)
+    local partId, stats = _G.SpinDrive.getPartStats(base)
+	base._spindrive_partId = partId
 	base._spindrive_stats = stats
 	base._spindrive_mult = stats and stats.rpm_pct_min or 1
 	base._spindrive_pct = 0
+	base._spindrive_angle = 0
 end)
 
 Hooks:PostHook(PlayerStandard, "update", "spindriveDt", function(self, t, dt)
@@ -40,6 +46,16 @@ Hooks:PostHook(PlayerStandard, "update", "spindriveDt", function(self, t, dt)
 			end
 			wpn._spindrive_pct = pct
 			wpn._spindrive_mult = mult
+			--spin
+			wpn._spindrive_angle = (wpn._spindrive_angle or 0) + stats.peak_rpm * (6 / stats.n_barrels) * pct * dt
+			local obj = wpn._parts[wpn._spindrive_partId].unit:orientation_object()
+			if stats.spin_axis == "x" then
+				obj:set_local_rotation(Rotation(wpn._spindrive_angle, 0, 0))
+			elseif stats.spin_axis == "y" then
+				obj:set_local_rotation(Rotation(0, wpn._spindrive_angle, 0))
+			else
+				obj:set_local_rotation(Rotation(0, 0, wpn._spindrive_angle))
+			end
 		end
 	end
 end)
