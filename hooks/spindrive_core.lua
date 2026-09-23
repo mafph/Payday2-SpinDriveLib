8af6110e7bfd4e16e1e7cde1a60ff6e8a1659bfa
Marcos Hebestreit <marcos.hebestreit@gmail.com>
2026-09-23 19:31:00 +0200

    done with FRM, tomorrow: spin

--- hooks/spindrive_core.lua
+++ hooks/spindrive_core.lua
@@ -12,51 +12,42 @@ function SD.getPartStats(weapon)
 	return nil
 end
 
-local function lerp(min, max, pct)
-	return min + ((max - min) * pct)
-end
+Hooks:OverrideFunction(NewRaycastWeaponBase, "fire_rate_multiplier", function(self, ...)
+    return self._fire_rate_multiplier * self._spindrive_mult
+end)
 
-local function rampFRM(weapon)
-	local stats = SD.getPartStats(weapon)
-	if not weapon._spindrive_pct then weapon._spindrive_pct = 0 end
-	if stats then
-		local desiredRPM = lerp(
-			stats.base_rpm * stats.rpm_pct_min,
-			stats.peak_rpm, 
-			weapon._spindrive_pct
-		)
-		return desiredRPM / stats.base_rpm
-	end
-
-	return nil
-end
-
-Hooks:PreHook(NewRaycastWeaponBase, "fire_rate_multiplier", "spindrive_FRM", function(self,...)
-	local FRM = rampFRM(self)
-	if FRM then
-		return FRM
-	end
-	return self._fire_rate_multiplier
+Hooks:PostHook(PlayerInventory, "_send_equipped_weapon", "spindrive_init", function(self,...)
+	local base = self:equipped_unit():base()
+    local stats = _G.SpinDrive.getPartStats(base)
+	base._spindrive_stats = stats
+	base._spindrive_mult = stats and stats.rpm_pct_min or 1
 end)
 
 Hooks:PostHook(PlayerStandard, "update", "spindriveDt", function(self, t, dt)
 	local SD = _G.SpinDrive
-	if self._equipped_unit then 
-		local wpn_unit = self._equipped_unit
-		if wpn_unit and wpn_unit:base() then 
-			local wpn = wpn_unit:base()
-			if not wpn._spindrive_pct then 
-				wpn._spindrive_pct = 0 
-			end
-			local _spindrive_stats = SD.getPartStats(wpn)
-			if _spindrive_stats then
-				if self._shooting then 
-					wpn._spindrive_pct = math.step(wpn._spindrive_pct, 1, dt / _spindrive_stats.accel_time)
-				elseif self:in_steelsight() then
-					wpn._spindrive_pct = math.step(wpn._spindrive_pct, _spindrive_stats.rpm_pct_ads or 0.4, dt / _spindrive_stats.accel_time)
-				else
-					wpn._spindrive_pct = math.step(wpn._spindrive_pct, 0, dt / _spindrive_stats.decel_time)
-				end
+	if self._equipped_unit then
+		local wpn = self._equipped_unit:base()
+		local stats = wpn._spindrive_stats
+		if stats then
+			local peak_mult = stats.peak_rpm / stats.base_rpm
+			if self._shooting then 
+				wpn._spindrive_mult = math.step(
+					wpn._spindrive_mult, 
+					peak_mult, 
+					(peak_mult - stats.rpm_pct_min) / stats.accel_time * dt
+				)
+			elseif self._state_data.in_steelsight then
+				wpn._spindrive_mult = math.step(
+					wpn._spindrive_mult,
+					stats.rpm_pct_min + stats.rpm_pct_ads * (peak_mult - stats.rpm_pct_min),
+					(peak_mult - stats.rpm_pct_min) / stats.accel_time * dt
+				)
+			else
+				wpn._spindrive_mult = math.step(
+					wpn._spindrive_mult, 
+					stats.rpm_pct_min, 
+					(peak_mult - stats.rpm_pct_min) / stats.decel_time * dt
+				)
 			end
 		end
 	end
