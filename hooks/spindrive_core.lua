c1d923bd2d07834bd09eeb860a8efc74594cef74
Marcos Hebestreit <marcos.hebestreit@gmail.com>
2026-09-23 03:55:26 +0200

    finalized rofbuff code

--- hooks/spindrive_core.lua
+++ hooks/spindrive_core.lua
@@ -1,13 +1,7 @@
 _G.SpinDrive = _G.SpinDrive or {}
 local SD = _G.SpinDrive
-local dt_sd = SD.dt
-local spinStart = SD.spinStart
-local spinTime = SD.spinTime
-local spinInc = SD.spinInc
-spinTime = nil
-spinInc = nil
 
-local function getPartStats(weapon)
+function SD.getPartStats(weapon)
 	for _, partId in pairs(weapon._blueprint) do
 		local part = tweak_data.weapon.factory.parts[partId]
 		local stats = (part.custom_stats or {}).spindrive
@@ -23,21 +17,23 @@ local function lerp(min, max, pct)
 end
 
 local function rampFRM(weapon)
-	
-	local pct = min(1, elapsed / accel_time)
-	
-	local stats = getPartStats(weapon)
+	local stats = SD.getPartStats(weapon)
+	if not weapon._spindrive_pct then weapon._spindrive_pct = 0 end
 	if stats then
-		local desiredRPM = lerp(stats.base_rpm * stats.rpm_pct_min, stats.peak_rpm, pct)
+		local desiredRPM = lerp(
+			stats.base_rpm * stats.rpm_pct_min,
+			stats.peak_rpm, 
+			weapon._spindrive_pct
+		)
 		return desiredRPM / stats.base_rpm
 	end
 
-	return 1
+	return nil
 end
 
 Hook:PreHook(NewRaycastWeaponBase, "fire_rate_multiplier", "spindrive_FRM", function(self,...)
 	local FRM = rampFRM(self)
-	if FRM > 1 then
+	if FRM then
 		return FRM
 	end
 	return self._fire_rate_multiplier
@@ -52,13 +48,24 @@ Hook:PostHook(NewRaycastWeaponBase, "_end_action_steelsight", "spindrive_deccel_
 	_G.SpinDrive.adsEndTime = t
 end)
 
-Hook:PostHook(PlayerStandard, "update", "spindriveDt", function(self,...)
-	if self._shooting = then 
-		_G.SpinDrive.spintime = t - _shooting_t
-		_G.SpinDrive.spinDownTime = 0
-	else 
-		_G.SpinDrive.spinDownTime = _G.SpinDrive.Downspintime + dt
-		_G.SpinDrive.spintime
+Hook:PostHook(PlayerStandard, "update", "spindriveDt", function(self, t, dt)
+	local SD = _G.SpinDrive
+	if self._equipped_unit then 
+		local wpn_unit = self._equipped_unit
+		if wpn_unit and wpn_unit:base() then 
+			local wpn = wpn_unit:base()
+			if not wpn._spindrive_pct then 
+				wpn._spindrive_pct = 0 
+			end
+			local _spindrive_stats = SD.getPartStats(wpn)
+			if _spindrive_stats then
+				if self._shooting then 
+					wpn._spindrive_pct = math.min(1, wpn._spindrive_pct + dt / _spindrive_stats.accel_time)
+				else 
+					wpn._spindrive_pct = math.max(0, wpn._spindrive_pct - dt / _spindrive_stats.decel_time)
+				end
+			end
+		end
 	end
 end)
 
