cc972ad61dfd80b2d97e7a4d67d508722d76703a
Marcos Hebestreit <marcos.hebestreit@gmail.com>
2026-09-23 02:46:10 +0200

    found a way to properly get time variable from playerstandard.update

--- hooks/spindrive_core.lua
+++ hooks/spindrive_core.lua
@@ -1,7 +1,13 @@
 _G.SpinDrive = _G.SpinDrive or {}
 local SD = _G.SpinDrive
+local dt_sd = SD.dt
+local spinStart = SD.spinStart
+local spinTime = SD.spinTime
+local spinInc = SD.spinInc
+spinTime = nil
+spinInc = nil
 
-function getPartStats(weapon)
+local function getPartStats(weapon)
 	for _, partId in pairs(weapon._blueprint) do
 		local part = tweak_data.weapon.factory.parts[partId]
 		local stats = (part.custom_stats or {}).spindrive
@@ -18,7 +24,7 @@ end
 
 local function rampFRM(weapon)
 	
-	pct
+	local pct = min(1, elapsed / accel_time)
 	
 	local stats = getPartStats(weapon)
 	if stats then
@@ -31,8 +37,28 @@ end
 
 Hook:PreHook(NewRaycastWeaponBase, "fire_rate_multiplier", "spindrive_FRM", function(self,...)
 	local FRM = rampFRM(self)
-	if FRM > 1
+	if FRM > 1 then
 		return FRM
 	end
 	return self._fire_rate_multiplier
-end
+end)
+
+
+Hook:PostHook(NewRaycastWeaponBase, "_start_action_steelsight", "spindrive_accel_ADS", function(self,...)
+	_G.SpinDrive.adsStartTime = t
+end)
+
+Hook:PostHook(NewRaycastWeaponBase, "_end_action_steelsight", "spindrive_deccel_ADS", function(self,...)
+	_G.SpinDrive.adsEndTime = t
+end)
+
+Hook:PostHook(PlayerStandard, "update", "spindriveDt", function(self,...)
+	if self._shooting = then 
+		_G.SpinDrive.spintime = t - _shooting_t
+		_G.SpinDrive.spinDownTime = 0
+	else 
+		_G.SpinDrive.spinDownTime = _G.SpinDrive.Downspintime + dt
+		_G.SpinDrive.spintime
+	end
+end)
+
